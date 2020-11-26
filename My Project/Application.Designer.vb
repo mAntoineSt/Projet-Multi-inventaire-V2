9e1e42a45e11dfa9b849b20cdbbb80ab2f01b226
mAntoineSt <antoine.professionnal@gmail.com>
2020-11-25 21:24:29 -0500

    Visuel version beau pour l'accueil

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Projet_Multi_inventaire_V2.Administration
+            Me.MainForm = Global.Projet_Multi_inventaire_V2.Accueil
         End Sub
     End Class
 End Namespace
